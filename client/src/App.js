af870d4bccd1a3275e35a9271c5836d834290e2f
andbret <and.bretnall@gmail.com>
2020-11-06 23:58:08 -0500

    Added Skeleton

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,27 @@
 import React from "react";
-<<<<<<< Updated upstream
-=======
+
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import LogSign from "./pages/Log-Sign";
 import {Container, Row, Col, Card, Form, Button } from "react-bootstrap";
 import { withRouter } from "react-router";
->>>>>>> Stashed changes
-
 import Display from "./components/Display";
 
 
 function App() {
   return (
-<<<<<<< Updated upstream
-    <div className="App">
+<>
+
+    
+   
+       
+              
+
+   <Display />
+   
+   
+    
+      <LogSign />
+      <div className="App">
       <div className="App-header">
         <h2>Welcome to React</h2>
       </div>
@@ -21,21 +29,9 @@ function App() {
         To get started, edit <code>src/App.js</code> and save to reload.
       </p>
     </div>
-=======
-    <>
-   
-       
-        
-          
-
-   <Display />
-   
-   
-    
-      <LogSign />
     
     </>
->>>>>>> Stashed changes
+
   );
 }
 
