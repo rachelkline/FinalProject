6d098cabe65ea487448816fec412783f37a53090
Rachel Kline <rach3lkline@gmail.com>
2020-11-09 15:41:56 -0500

    Working User database

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,29 +1,19 @@
 import React from "react";
-import { Link, useLocation } from "react-router-dom";
+import { Route, Link, useLocation } from "react-router-dom";
+import LoginForm from "../components/log-in"
 
 
 function LogIn() {
-    const location = useLocation();
+    // const location = useLocation();
     return (
         <div className="container">
-
-            <h1>Log In</h1>
-            <form className="login">
-                <div className="row">
-                    <div className="input-field">
-                        <input id="email-input" type="text" className="validate" placeholder="Email Address" />
-                    </div>
-                    <div className="input-field">
-                        <input id="password-input" type="text" className="validate" placeholder="Password" />
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col s2">
-                        <button type="submit" class="btn"><Link to="/dashboard" className={location.pathname === "/signup"}>sign in</Link></button>
-                    </div>
-                    <button type="submit" class="btn"><Link to="/signup" className={location.pathname === "/signup"}>sign up</Link></button>
-                </div>
-            </form>
+        <Route
+          path="/login"
+          render={() =>
+            <LoginForm
+            //   updateUser={this.updateUser}
+            />}
+        />
         </div>
 
     )
