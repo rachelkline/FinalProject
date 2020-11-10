71603680e719c7225e3b2b60f23cb2c146a080c0
andbret <and.bretnall@gmail.com>
2020-11-10 10:16:19 -0500

    fixed collapse bug

--- client/src/components/Friends/Friends.js
+++ client/src/components/Friends/Friends.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "./style.css";
 import { Container, Row, Col, Card, Button, Form, Badge } from "react-bootstrap";
-import {Collapse} from 'react-collapse';
+import { Collapse } from 'react-collapse';
 import FriendLedger from "../FriendLedger/FriendLedger";
 // const {Collapse, UnmountClosed} = ReactCollapse;
 
@@ -10,64 +10,79 @@ const friends = [
     { name: 'Bill Billerson', value: '2' },
     { name: 'Frank Smith', value: '3' },
     { name: 'Greg Smith', value: '4' },
-  ];
+];
 
 class Nested extends React.PureComponent {
     constructor(props) {
-      super(props);
-      this.state = {isOpened: false};
+        super(props);
+        let isOpened = {};
+        friends.map((friend, i) => isOpened['friend'+i] = false)
+        this.state = { isOpened: isOpened }
     }
-  
-  
+
+
     render() {
         ;
+
         
-      const {isOpened} = this.state;
-    //   const [radioValue, setRadioValue] = useState('1');
+        // const { isOpened, test } = this.state;
+        //   const [radioValue, setRadioValue] = useState('1');
 
-      return (
-        <>
-                          {friends.map((friend, i) => (
+        return (
+            <>
+                {friends.map((friend, i) => (
 
-            <Card className="shadow mb-3">
-  <Card.Body>
-          <div className="config">
-            
+                    <Card className="shadow mb-3">
+                        <Card.Body>
+                            <div className="config">
 
-            <Card.Title style={{ display: "flex"}}><strong>{friend.name}</strong>
 
-            <Badge className="ml-2" variant="danger">You owe $35</Badge>{' '}
-              <input 
-              friends={friends[i]}
-                className="input pull-right"
-                key={i}
-                name="friend"
-                type="checkbox"
-                value={friend.name}
-                onClick={this.handleClick}
-          
-                style={{ marginLeft: "auto" }}
-                checked={i.isOpened}
-                onChange={({target: {checked}}) => this.setState({isOpened: checked})} />
-                </Card.Title>
-            
-            
-          </div>
-  
-          <Collapse 
-       key={i}
-      
-     
-       isOpened={isOpened}>
-          <div className="subCollapse">
-            <FriendLedger/>
-        </div>
-          </Collapse>
-          </Card.Body>
-</Card>
-       ))}
-        </>
-      );
+                                <Card.Title style={{ display: "flex" }}><strong>{friend.name}</strong>
+
+                                    <Badge className="ml-2" variant="danger">You owe $35</Badge>{' '}
+                                    <input
+                                        friends={friends[i]}
+                                        className="input pull-right"
+                                        key={i}
+                                        name="friend"
+                                        type="checkbox"
+                                        value={friend.name}
+                                        onClick={this.handleClick}
+
+                                        style={{ marginLeft: "auto" }}
+                                        checked={i.isOpened}
+                                        onChange={({ target: { checked } }) => {
+                                            // let isOpenedNew = isOpened;
+                                            // isOpenedNew[i]= checked;
+                                            let newState = {}
+                                            newState['friend'+i] = checked
+                                            this.setState(newState)
+
+                                            // console.log(isOpenedNew);
+                                            // console.log(isOpened[i]);
+                                        }
+                                        
+                                        } />
+                                </Card.Title>
+
+
+                            </div>
+
+                            <Collapse
+                                key={i}
+
+
+                                isOpened={this.state['friend'+i]}>
+                                <div className="subCollapse">
+                         
+                                    <FriendLedger />
+                                </div>
+                            </Collapse>
+                        </Card.Body>
+                    </Card>
+                ))}
+            </>
+        );
     }
-  }
-  export default Nested;
+}
+export default Nested;
