f0ec0e0b1d07081ac229730d6ea472fa4f2b6443
Ryan <69523694+Rseckman@users.noreply.github.com>
2020-11-11 13:50:35 -0500

    formatted some pages

--- client/src/components/Itinerary/Itinerary.js
+++ client/src/components/Itinerary/Itinerary.js
@@ -5,114 +5,128 @@ import Events from "./events";
 import AddEvent from "./addEvent";
 // import Demo3 from "./demo3";
 var i;
-Date.prototype.addDays = function(days) {
-    var dat = new Date(this.valueOf())
-    dat.setDate(dat.getDate() + days);
-    return dat;
-}
+Date.prototype.addDays = function (days) {
+  var dat = new Date(this.valueOf());
+  dat.setDate(dat.getDate() + days);
+  return dat;
+};
 
 function getDates(startDate, stopDate) {
-   var dateArray = new Array();
-   var currentDate = startDate;
-   while (currentDate <= stopDate) {
-     dateArray.push(currentDate)
-     currentDate = currentDate.addDays(1);
-   }
-   return dateArray;
- }
- var dateArray = getDates(new Date(), (new Date()).addDays(2));
- for (i = 0; i < dateArray.length; i ++ ) {
-     var months = [
- 'January',
- 'February',
- 'March',
- 'April',
- 'May',
- 'June',
- 'July',
- 'August',
- 'September',
- 'October',
- 'November',
- 'December'
- ];
-     var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
- var d = dateArray[i].getDay();
- var dayName = days[d];
- var m = dateArray[i].getMonth();
- var monthName = months[m];
+  var dateArray = new Array();
+  var currentDate = startDate;
+  while (currentDate <= stopDate) {
+    dateArray.push(currentDate);
+    currentDate = currentDate.addDays(1);
+  }
+  return dateArray;
+}
+var dateArray = getDates(new Date(), new Date().addDays(2));
+for (i = 0; i < dateArray.length; i++) {
+  var months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  var days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  var d = dateArray[i].getDay();
+  var dayName = days[d];
+  var m = dateArray[i].getMonth();
+  var monthName = months[m];
   console.log(dateArray[i]);
   console.log(dateArray[i].getDate());
- var newDate = dateArray[i].getDate();
+  var newDate = dateArray[i].getDate();
   console.log(dayName);
   console.log(monthName);
   console.log(dateArray[i].getFullYear());
 }
 
-
 const dayss = [
-    { name: 'Monday', date: '1', month: 'November 2020' },
-    { name: 'Tuesday', date: '2', month: 'November 2020' },
-    { name: 'Wednesday', date: '3', month: 'November 2020' },
-    { name: 'Thursday', date: '4', month: 'November 2020' },
-    { name: 'Friday', date: '5', month: 'November 2020' }
-  ];
-  const events = [
-    [{ title: 'Do the thing', location: 'Delaware', time: '4:00pm' },
-    { title: 'Do the thing', location: 'Delaware', time: '4:00pm' }],
-    { title: 'Do the thing', location: 'Delaware', time: '4:00pm' },
-    { title: 'Do the thing', location: 'Delaware', time: '4:00pm' },
-    { title: 'Do the thing', location: 'Delaware', time: '4:00pm' },
-  ];
-  
+  { name: "Monday", date: "1", month: "November 2020" },
+  { name: "Tuesday", date: "2", month: "November 2020" },
+  { name: "Wednesday", date: "3", month: "November 2020" },
+  { name: "Thursday", date: "4", month: "November 2020" },
+  { name: "Friday", date: "5", month: "November 2020" },
+];
+const events = [
+  [
+    { title: "Do the thing", location: "Delaware", time: "4:00pm" },
+    { title: "Do the thing", location: "Delaware", time: "4:00pm" },
+  ],
+  { title: "Do the thing", location: "Delaware", time: "4:00pm" },
+  { title: "Do the thing", location: "Delaware", time: "4:00pm" },
+  { title: "Do the thing", location: "Delaware", time: "4:00pm" },
+];
+
 class Itinerary extends React.Component {
   constructor(props) {
-
     super(props);
     this.state = {
       name: "React",
       showHideDemo1: true,
       showHideDemo2: false,
-       dateSelected: {},
-       newDate:[] 
+      dateSelected: {},
+      newDate: [],
     };
-
 
     this.hideComponent = this.hideComponent.bind(this);
     this.handleClick = this.handleClick.bind(this);
   }
-componentDidMount(){
-    var dateArray = getDates(new Date(), (new Date()).addDays(2));
-for (i = 0; i < dateArray.length; i ++ ) {
-    var months = [
-'January',
-'February',
-'March',
-'April',
-'May',
-'June',
-'July',
-'August',
-'September',
-'October',
-'November',
-'December'
-];
-    var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-var d = dateArray[i].getDay();
-var dayName = days[d];
-var m = dateArray[i].getMonth();
-var monthName = months[m];
- console.log(dateArray[i]);
- console.log(dateArray[i].getDate());
-var newDate = dateArray[i].getDate();
- console.log(dayName);
- console.log(monthName);
- console.log(dateArray[i].getFullYear());
-}
-this.setState({newDate: newDate})
-
-}
+  componentDidMount() {
+    var dateArray = getDates(new Date(), new Date().addDays(2));
+    for (i = 0; i < dateArray.length; i++) {
+      var months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+      ];
+      var days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+      ];
+      var d = dateArray[i].getDay();
+      var dayName = days[d];
+      var m = dateArray[i].getMonth();
+      var monthName = months[m];
+      console.log(dateArray[i]);
+      console.log(dateArray[i].getDate());
+      var newDate = dateArray[i].getDate();
+      console.log(dayName);
+      console.log(monthName);
+      console.log(dateArray[i].getFullYear());
+    }
+    this.setState({ newDate: newDate });
+  }
   hideComponent(name) {
     console.log(name);
     switch (name) {
@@ -125,59 +139,87 @@ this.setState({newDate: newDate})
         this.setState({ showHideDemo1: !this.state.showHideDemo1 });
         this.setState({ showHideDemo2: !this.state.showHideDemo2 });
         break;
-
     }
   }
-  handleClick(event){
-      event.preventDefault()
+  handleClick(event) {
+    event.preventDefault();
     //   this.setState()
-    console.log("test")
-      console.log(event.target)
+    console.log("test");
+    console.log(event.target);
   }
   render() {
     const { showHideDemo1, showHideDemo2 } = this.state;
     return (
       <div>
         <Card className="shadow">
+          <Card.Body>
+            <Card.Title style={{ display: "flex" }}>
+              <strong>Tuesday</strong>
+              {showHideDemo1 && (
+                <Button
+                  style={{ marginLeft: "auto" }}
+                  onClick={() => this.hideComponent("showHideDemo1")}
+                  className="pull-right"
+                  size="sm"
+                  variant="secondary"
+                >
+                  +
+                </Button>
+              )}
+            </Card.Title>
 
-        <Card.Body>
-              
-          <Card.Title style={{ display: "flex" }}><strong>Tuesday</strong>{showHideDemo1 && <Button style={{ marginLeft: "auto" }} onClick={() => this.hideComponent("showHideDemo1")} className="pull-right" size="sm" variant="secondary">+</Button>}
-</Card.Title>
-          
-      <Card.Subtitle className="mb-2 text-muted">November 2020</Card.Subtitle>
-       
-      {/* {this.state.dates.map((item, index) => ( */}
-        
-<Nav variant="pills" defaultActiveKey="/">
-{dayss.map((day, i) => (
-<Nav.Item>
-        {showHideDemo1 &&<Dates date={day.date} onClick={() =>(this.handleClick)}
-    // date={item.date}
-/>}
-</Nav.Item>
+            <Card.Subtitle className="mb-2 text-muted">
+              November 2020
+            </Card.Subtitle>
 
-))}
-</Nav>
+            {/* {this.state.dates.map((item, index) => ( */}
 
-<hr/>
-      {/* {this.state.dates.map((item, index) => ( */}
+            <Nav variant="pills" defaultActiveKey="/">
+              {dayss.map((day, i) => (
+                <Nav.Item>
+                  {showHideDemo1 && (
+                    <Dates
+                      date={day.date}
+                      onClick={() => this.handleClick}
+                      // date={item.date}
+                    />
+                  )}
+                </Nav.Item>
+              ))}
+            </Nav>
 
-        {showHideDemo1 &&<Events
-    // title={item.title}
-    // location={item.location}
-    // time={item.time}
+            <hr />
+            {/* {this.state.dates.map((item, index) => ( */}
 
-/>}
-{/* ))} */}
+            {showHideDemo1 && (
+              <Events
+              // title={item.title}
+              // location={item.location}
+              // time={item.time}
+              />
+            )}
+            {/* ))} */}
 
-          {showHideDemo2 && <AddEvent />}
-          {showHideDemo2 && <Button className="mr-2" onClick={() => this.hideComponent("showHideDemo1")} variant="outline-primary">Cancel</Button>}
-            {showHideDemo2 && <Button onClick={() => this.hideComponent("showHideDemo1")} variant="primary">Confirm</Button>} 
-
+            {showHideDemo2 && <AddEvent />}
+            {showHideDemo2 && (
+              <Button
+                className="mr-2"
+                onClick={() => this.hideComponent("showHideDemo1")}
+                variant="outline-primary"
+              >
+                Cancel
+              </Button>
+            )}
+            {showHideDemo2 && (
+              <Button
+                onClick={() => this.hideComponent("showHideDemo1")}
+                variant="primary"
+              >
+                Confirm
+              </Button>
+            )}
           </Card.Body>
         </Card>
-
       </div>
     );
   }
