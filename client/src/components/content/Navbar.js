639548b9a78ebe8f912abd939bd9f239b8e25732
andbret <and.bretnall@gmail.com>
2020-11-12 17:31:40 -0500

    nav

--- client/src/components/content/Navbar.js
+++ client/src/components/content/Navbar.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { Component, useContext } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Route, Link } from "react-router-dom";
+
 import { faAlignLeft } from "@fortawesome/free-solid-svg-icons";
 import { Navbar, Button, Nav } from "react-bootstrap";
+import axios from "axios";
+import {AuthContext} from '../../contexts/auth-provider';
+
 import "./nav.css";
 
 class NavBar extends React.Component {
+  
   render() {
+    
     return (
       <Navbar
         bg="light"
@@ -20,6 +27,11 @@ class NavBar extends React.Component {
  
       Travel Buddy
     </Navbar.Brand>
+    <Nav className="justify-content-end ml-auto">
+    <Nav.Item>
+      <Nav.Link className="colorWhite" href="/home">Please put LOGOUT here</Nav.Link>
+    </Nav.Item>
+  </Nav>
       </Navbar>
       
     );
